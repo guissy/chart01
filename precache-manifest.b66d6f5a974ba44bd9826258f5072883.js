04d0ed6bf7e43e2e54404edbd44700e4e0cb4c0e
桂子 <guissy@qq.com>
2019-08-25 14:46:56 +0800

    Updates

--- precache-manifest.b66d6f5a974ba44bd9826258f5072883.js
+++ precache-manifest.b66d6f5a974ba44bd9826258f5072883.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06e80f0c434022e49175b007c58d191f",
+    "revision": "294e43f7770f34f4b20a426578af2820",
     "url": "/chart01/index.html"
   },
   {
-    "revision": "6e01fea057ea9cf53cbd",
-    "url": "/chart01/static/css/main.9c9adbed.chunk.css"
+    "revision": "208223df83ca3164bb97",
+    "url": "/chart01/static/css/main.f19fdf71.chunk.css"
   },
   {
-    "revision": "40d827d7dafcd9fe113e",
-    "url": "/chart01/static/js/2.c92646ff.chunk.js"
+    "revision": "d45dcee38b9dc08c3b4d",
+    "url": "/chart01/static/js/2.fc0a8d63.chunk.js"
   },
   {
-    "revision": "6e01fea057ea9cf53cbd",
-    "url": "/chart01/static/js/main.6db4eaca.chunk.js"
+    "revision": "208223df83ca3164bb97",
+    "url": "/chart01/static/js/main.331f2c63.chunk.js"
   },
   {
     "revision": "9e9b7a4e8c016c7326b2",
