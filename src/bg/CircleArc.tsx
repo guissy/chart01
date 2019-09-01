a2e4c6320fbf90d4b6a2c9c59fd49e29a1fea23f
桂子 <guissy@qq.com>
2019-09-01 15:29:03 +0800

    header bg

--- src/bg/CircleArc.tsx
+++ src/bg/CircleArc.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CircleArc.css';
+import line3 from '../assets/line3.svg';
 
 interface Props {
 }
@@ -14,11 +15,13 @@ const CircleArc: React.FC<Props> = ({}) => {
         <div className="arc4"></div>
         {
           Array(14).fill(0).map((_, i) =>
-            <div key={i} className="rect1" style={{transform: 'translateX(49px) rotate('+i*12+'deg)'}}></div>)
+            <div key={i} className="rect1" style={{ transform: 'translateX(49px) rotate(' + i * 12 + 'deg)' }}></div>)
         }
-      <div className="arc5"></div>
+        <div className="arc5"></div>
+      </div>
+      <img src={line3}/>
+      <img src={line3} className="right"/>
     </div>
-  </div>
   );
 };
 
