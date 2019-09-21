985f9365b74fd9f834b33b5f0248329b1efe6e3e
桂子 <guissy@qq.com>
2019-09-21 19:23:24 +0800

    unit test snap ok

--- src/earth/Earth.test.tsx
+++ src/earth/Earth.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Earth from './Earth';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Earth />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('正确的渲染', () => {
+  const tree = renderer
+    .create(<Earth />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
